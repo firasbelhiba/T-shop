8e4bbedf201e02b0ce76de9d1009dc1ad90e8397
firasbelhiba <firas.belhiba@esprit.tn>
2022-01-23 23:23:00 +0100

    Fixing Loading script error

--- components/layout/base/index.js
+++ components/layout/base/index.js
@@ -1,4 +1,5 @@
 import { Footer, Navbar } from "@components/shared";
+import Script from "next/script";
 
 export default function BaseLayout({ children }) {
   return (
@@ -7,30 +8,37 @@ export default function BaseLayout({ children }) {
       {children}
       <Footer />
 
-      {/* <Script src="assets/js/vendor/jquery-3.4.1.min.js" />
-        <Script src="assets/js/popper.js" />
-        <Script src="assets/js/bootstrap.min.js" />
-        <Script src="assets/js/owl.carousel.min.js" />
-        <Script src="assets/js/slick.min.js" />
-        <Script src="assets/js/jquery.magnific-popup.min.js" />
-        <Script src="assets/js/jquery.countdown.js" />
-        <Script src="assets/js/jquery.ui.js" />
-        <Script src="assets/js/jquery.elevatezoom.js" />
-        <Script src="assets/js/isotope.pkgd.min.js" />
-        <Script src="assets/js/plugins.js" />
-        <Script src="assets/js/main.js" /> */}
-      <script src="assets/js/vendor/jquery-3.4.1.min.js"></script>
-      <script src="assets/js/popper.js"></script>
-      <script src="assets/js/bootstrap.min.js"></script>
-      <script src="assets/js/owl.carousel.min.js"></script>
-      <script src="assets/js/slick.min.js"></script>
-      <script src="assets/js/jquery.magnific-popup.min.js"></script>
-      <script src="assets/js/jquery.countdown.js"></script>
-      <script src="assets/js/jquery.ui.js"></script>
-      <script src="assets/js/jquery.elevatezoom.js"></script>
-      <script src="assets/js/isotope.pkgd.min.js"></script>
-      <script src="assets/js/plugins.js"></script>
-      <script src="assets/js/main.js"></script>
+      <Script
+        src="assets/js/vendor/jquery-3.4.1.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script src="assets/js/popper.js" strategy="beforeInteractive" />
+      <Script src="assets/js/bootstrap.min.js" strategy="beforeInteractive" />
+      <Script src="assets/js/slick.min.js" strategy="beforeInteractive" />
+      <Script
+        src="assets/js/jquery.magnific-popup.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        src="assets/js/owl.carousel.min.js"
+        strategy="beforeInteractive"
+      />
+
+      <Script
+        src="assets/js/jquery.countdown.js"
+        strategy="beforeInteractive"
+      />
+      <Script src="assets/js/jquery.ui.js" strategy="beforeInteractive" />
+      <Script
+        src="assets/js/jquery.elevatezoom.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        src="assets/js/isotope.pkgd.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script src="assets/js/plugins.js" strategy="beforeInteractive" />
+      <Script src="assets/js/main.js" strategy="beforeInteractive" />
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { Navbar, Footer, Breadcrumb, OrderProduct } from "@components/shared";
 import { Carousel, BannerStart } from "@components/Home";
 import { WalletInfo, EthPriceDisplay } from "@components/web3";
 import { Card } from "@components/order";
-import Script from "next/script";
 import { BaseLayout } from "@components/layout";
 
 export default function Home() {
