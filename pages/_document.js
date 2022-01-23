442086bfd1cebae263e9f87c2cc2fdb532750af7
firasbelhiba <firas.belhiba@esprit.tn>
2022-01-23 16:56:46 +0100

    Fixing deployment errors

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script'
+
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { Navbar, Footer, Breadcrumb , OrderProduct} from "@components/shared";
 import { Carousel, BannerStart } from "@components/Home";
 import { WalletInfo, EthPriceDisplay } from "@components/web3";
+import Script from 'next/script'
+
 
 export default function Home() {
   return (
@@ -12,20 +14,20 @@ export default function Home() {
       <Breadcrumb />
       <Footer />
 
-      <script src="assets/js/vendor/jquery-3.4.1.min.js"></script>
+      <Script src="assets/js/vendor/jquery-3.4.1.min.js"></Script>
 
-      <script src="assets/js/popper.js"></script>
-      <script src="assets/js/bootstrap.min.js"></script>
-      <script src="assets/js/owl.carousel.min.js"></script>
-      <script src="assets/js/slick.min.js"></script>
-      <script src="assets/js/jquery.magnific-popup.min.js"></script>
-      <script src="assets/js/jquery.countdown.js"></script>
-      <script src="assets/js/jquery.ui.js"></script>
-      <script src="assets/js/jquery.elevatezoom.js"></script>
-      <script src="assets/js/isotope.pkgd.min.js"></script>
-      <script src="assets/js/plugins.js"></script>
+      <Script src="assets/js/popper.js"></Script>
+      <Script src="assets/js/bootstrap.min.js"></Script>
+      <Script src="assets/js/owl.carousel.min.js"></Script>
+      <Script src="assets/js/slick.min.js"></Script>
+      <Script src="assets/js/jquery.magnific-popup.min.js"></Script>
+      <Script src="assets/js/jquery.countdown.js"></Script>
+      <Script src="assets/js/jquery.ui.js"></Script>
+      <Script src="assets/js/jquery.elevatezoom.js"></Script>
+      <Script src="assets/js/isotope.pkgd.min.js"></Script>
+      <Script src="assets/js/plugins.js"></Script>
 
-      <script src="assets/js/main.js"></script>
+      <Script src="assets/js/main.js"></Script>
     </div>
   );
 }
