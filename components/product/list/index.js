4e078dcde3285e6161b32c9ef6ee1048ac568fde
firasbelhiba <firas.belhiba@esprit.tn>
2022-01-24 03:15:55 +0100

    Fixing deployment errors ( Missing "key" prop for element in iterator )

--- components/product/list/index.js
+++ components/product/list/index.js
@@ -7,27 +7,29 @@ export default function List({ merch }) {
       <div className="responsive-container">
         <div className="grid">
           {merch.map((m) => (
-            <Link href={`/merch/${m.slug}`}>
-              <a key={m.id} rel="noopener noreferrer" className="product">
-                <div className="product-image">
-                  <Image
-                    src={m.coverImage}
-                    alt={m.title}
-                    width="400"
-                    height="400"
-                  />
-                </div>
-                <div className="product-content">
-                  <div className="product-info">
-                    <h2 className="product-title">Cosmonaut</h2>
-                    <p className="product-price">$ 10</p>
+            <div key={m.id}>
+              <Link href={`/merch/${m.slug}`}>
+                <a rel="noopener noreferrer" className="product">
+                  <div className="product-image">
+                    <Image
+                      src={m.coverImage}
+                      alt={m.title}
+                      width="400"
+                      height="400"
+                    />
                   </div>
-                  <button className="product-action">
-                    <i className="material-icons-outlined">favorite_border</i>
-                  </button>
-                </div>
-              </a>
-            </Link>
+                  <div className="product-content">
+                    <div className="product-info">
+                      <h2 className="product-title">Cosmonaut</h2>
+                      <p className="product-price">$ 10</p>
+                    </div>
+                    <button className="product-action">
+                      <i className="material-icons-outlined">favorite_border</i>
+                    </button>
+                  </div>
+                </a>
+              </Link>
+            </div>
           ))}
         </div>
       </div>
