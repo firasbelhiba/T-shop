3a0179ede83e759a04563620312b9f305497a06c
firasbelhiba <firas.belhiba@esprit.tn>
2022-01-26 01:34:47 +0100

    detecting current network

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
+import useSWR from "swr";
+
+const NETWORK = {
+  1: "Main Ethereum network",
+  3: "Ropsten test network",
+  4: "Rinkeby test network",
+  5: "Goerli test network",
+  42: "Kovan test netwoek",
+  56: "Binance Smart Chain",
+  1337: "Ganache",
+};
+
 export const handler = (web3, provider) => () => {
+  const { mutate, ...swrResponse } = useSWR(
+    () => (web3 ? "web3/network" : null),
+    async () => {
+      const chainId = await web3.eth.getChainId();
+      return NETWORK[chainId];
+    }
+  );
+  useEffect(() => {
+    provider &&
+      provider.on("chainChanged", (chainId) => mutate(parseInt(chainId, 16)));
+  }, [web3]);
   return {
     network: {
-      data: "Testing network",
+      mutate,
+      ...swrResponse,
     },
   };
 };
