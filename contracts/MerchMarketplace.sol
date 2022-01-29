6bc79cbc58a00c3f46d054caea225bb431a88e5b
firasbelhiba <firas.belhiba@esprit.tn>
2022-01-29 17:33:32 +0100

    purchaseMerch function

--- contracts/MerchMarketplace.sol
+++ contracts/MerchMarketplace.sol
@@ -9,10 +9,31 @@ contract MerchMarketPlace {
     }
 
     struct Merch {
-        uint id;
-        uint price;
-        bytes32 zkproof;
-        address owner; 
-        State state;
+        uint256 id; // 32
+        uint256 price; // 32
+        bytes32 zkproof; // 32
+        address owner; // 20
+        State state; // 1
+    }
+
+    // Mapping of merchHash to merchData
+    mapping(bytes32 => Merch) private ownedMerch;
+
+    // Mapping of merchId to merchHash
+    mapping(uint256 => bytes32) private ownedMerchHash;
+
+    uint256 private totalOwnedMerch;
+
+    function purchaseMerch(bytes16 merchId, bytes32 zkproof) external payable {
+        bytes32 merchHash = keccak256(abi.encodePacked(merchId, msg.sender));
+        uint256 id = totalOwnedMerch++;
+        ownedMerchHash[id] = merchHash;
+        ownedMerch[merchHash] = Merch({
+            id: id,
+            price: msg.value,
+            zkproof: zkproof,
+            owner: msg.sender,
+            state: State.Purchased
+        });
     }
 }
